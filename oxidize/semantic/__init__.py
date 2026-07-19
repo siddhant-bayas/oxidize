3982a0d6193c839bfdb4179f5dd08b2e3c6fe285
sidx0 <siddhantbayas5@gmail.com>
2026-07-19 11:23:07 +0530

    update semantic exports

--- oxidize/semantic/__init__.py
+++ oxidize/semantic/__init__.py
@@ -1,7 +1,13 @@
 from __future__ import annotations
 
 from oxidize.semantic.differ import SemanticChange, format_semantic_diff, semantic_diff
-from oxidize.semantic.entities import Entity, EntityType, EntitySnapshot, extract_entities
+from oxidize.semantic.entities import (
+    Entity,
+    EntityType,
+    EntitySnapshot,
+    extract_entities,
+)
+from oxidize.semantic.parser import HAS_TREE_SITTER
 
 __all__ = [
     "Entity",
@@ -11,4 +17,5 @@ __all__ = [
     "SemanticChange",
     "semantic_diff",
     "format_semantic_diff",
+    "HAS_TREE_SITTER",
 ]
